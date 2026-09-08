100edee2a491235c7cdb74a5ca601e506fb28166
DEV SINGH <devsingh120307@gmail.com>
2026-09-08 10:06:12 +0530

    add helper method

--- lab3/app.js
+++ lab3/app.js
@@ -1,12 +1,45 @@
 import http from "http";
+// import * as teams from 'teams.js'
+import { getAllTeams } from "./teams.js";
 
+const PORT = 5000;
+
+const sendJson = (res, statusCode, data)=>{
+    res.writehead(statusCode,{"content-type":"application/json"});
+    res.end(data === "undefined" ? "" : JSON.stringify(data));
+};
+
+const parseJSONBody = (req)=>{
+    new Promise((reqsolve,reject)=>{
+        let body = ''
+        req.on('data',(chunk)=>{
+            body += chunk.toString();
+        });
+        req.on("end",()=>{
+            try {
+                resolve(body ? JSON.parse(body) : {});
+            }
+            catch (error){
+                reject(error);
+            }
+        });
+    });
+};
 const server = http.createServer();
 server.on('request',(req,res) => {
-    // res.write("<h1>welcome to server side programming</h1>");
-    // res.write("<h2>nodemon is tracking the files</h2>");
-    res.end("<h1>SIH Interal</h1>");
+    if(req.url ==='/' && req.method == 'GET'){
+        const teams = getAllTeams();
+        console.log("Teams:", teams);
+        
+        res.write(JSON.stringify(teams));
+    }
+    else{
+        res.statusCode = 404;
+    }
+
+    res.end();
 });
 
-server.listen(5000, () =>{
-    console.log("server is running"); 
+server.listen(PORT, () =>{
+    console.log("SIH server is running at", PORT); 
 });
