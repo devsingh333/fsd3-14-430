0a664e0ab7b94e1a7e8a4eb93f8bb08a68bcf307
DEV SINGH <devsingh120307@gmail.com>
2026-09-08 09:37:36 +0530

    Add teams.js

--- lab3/app.js
+++ lab3/app.js
@@ -2,9 +2,9 @@ import http from "http";
 
 const server = http.createServer();
 server.on('request',(req,res) => {
-    res.write("<h1>welcome to server side programming</h1>");
-    res.write("<h2>nodemon is tracking the files</h2>");
-    res.end();
+    // res.write("<h1>welcome to server side programming</h1>");
+    // res.write("<h2>nodemon is tracking the files</h2>");
+    res.end("<h1>SIH Interal</h1>");
 });
 
 server.listen(5000, () =>{
