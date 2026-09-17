f17373cc1c1f8e3c9c0789615300cd3a3cc1149f
DEV SINGH <devsingh120307@gmail.com>
2026-09-17 14:01:01 +0530

    user login

--- lab5/app.js
+++ lab5/app.js
@@ -10,4 +10,24 @@ app.get("/", (req,res) => {
 //     res.send("route not found");
 // });
 
-app.listen(3000, () => console.log("Server is running"));
+app.get('/about', (req, res) => {
+    res.send("We are FSD developers");
+});
+
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'});
+});
+
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg : 'user update'});
+});
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg : 'remove user 1'});
+});
+
+app.use((req,res)=>{
+    res.status(404).send("Not Found");
+});
+
+app.listen(3333, () => console.log("Server is running"));
---
+++ lab5/readme.md
@@ -0,0 +1,16 @@
+# Express
+Fast, unopinionated, minimalist web framework for Node.js
+
+1. create npm project `npm init -y`
+2. update package.json
+`type : module`
+3. install nodemon with `npm i nodemon -D`
+4. install express with `npm i express`
+5. update package.json
+
+```
+"scripts": {
+    "start":"node app.js",
+    "dev":"nodemon app.js"
+},
+```
